fe35f8cb86e79bacd3b2732918c70270f7ed3c99
R.Zelenev <r.zelenev@1440.space>
2025-10-03 17:40:34 +0300

    D0 new

---
+++ Lesson_7/D0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+typedef struct list
+{
+    char word [256];
+    struct list *next; 
+} list_t;
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
