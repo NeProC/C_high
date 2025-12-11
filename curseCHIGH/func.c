33e5aff7624f1a81c913bea9e42bc3f17ba12762
R.Zelenev <r.zelenev@1440.space>
2025-12-11 17:38:18 +0300

    add Make

--- curseCHIGH/func.c
+++ curseCHIGH/func.c
@@ -1,6 +1,9 @@
+#include "func.h"
 
-
-typedef float(*function)(float);
+int signF(float x, function f)
+{
+    return f(x) == 0 ? 0 : (f(x) < 0 ? -1 : +1);
+}
 
 float rootFindLineSearch(float xl, float xr, float eps,function f)
 {
@@ -15,9 +18,196 @@ float rootFindLineSearch(float xl, float xr, float eps,function f)
     
 }
 
+float rootFindDiv(float xl, float xr, float eps,function f)
+{
+    unsigned int stepCount = 0;
+    float xMid;
+    while (fabs(xr-xl) > eps)
+    {
+        stepCount++;
+        xMid = (xl+xr)/2;
+        if(signF(xl,f) != signF(xMid,f))
+            xr = xMid;
+        else
+            xl = xMid;
+    }
+    printf("Find root for %d steps\n",stepCount); 
+    return (xl+xr) /2;
+}
 
+float rootFindDiv2(float xl, float xr, float eps, function f) 
+{
+    unsigned int stepCount=0; //число шагов
+    float xm;
+    while(fabs(xr-xl)>eps) {
+            //вещественный модуль разницы
+        stepCount++;
+        xm=(xl+xr)/2; // середина отрезка
+        if(f(xr)==0) { // нашли решение на правой границе
+            printf("Find root for %d steps\n",stepCount);
+            return xr;
+        }
+        else if(f(xl)==0) { // нашли решение на левой границе
+            printf("Find root for %d steps\n",stepCount);
+            return xl;
+        }
+        if(signF(xl,f) != signF(xm,f))//если знак отличается
+            xr=xm;
+        else
+            xl=xm;
+    }
+    printf("Find root for %d steps\n",stepCount); //статистика
+    return (xl+xr)/2;
+}
+
+float rootFindChord(float xl, float xr, float eps, function f)
+{
+    unsigned int stepCount = 0;
+    while (fabs(xr - xl) > eps)
+    {
+        stepCount++;
+        xl = xr - (xr - xl) * f(xr) / (f(xr) - f(xl));
+        xr = xl - (xl - xr) * f(xl) / (f(xl) - f(xr));
+    }
+    printf("Find root for %d steps\n",stepCount); //статистика
+    return xr;
+}
+
+float rootFindTangent(float xn, float eps, function f, function df)
+{
+    float xl = xn - f(xn)/df(xn);
+    float x0 = xn;
+    unsigned int stepCount = 0;
+    while (fabs(x0-xl) > eps)
+    {
+        stepCount++;
+        x0 = xl;
+        xl = xl - f(xl)/df(xl);
+    }
+    printf("Find root for %d steps\n",stepCount); //статистика
+    return xl;
+}
+
+float rootFindCombine(float xl, float xr, float eps, function f, function df, function ddf)
+{
+    unsigned int stepCount = 0;
+    while (fabs(xl-xr) > 2*eps) {
+        stepCount++;
+        if(f(xl)*ddf(xl) < 0){
+            xl = xl - (f(xl) * (xl - xr) / (f(xl) - f(xr)));
+        }
+        else{
+            xl = xl - f(xl)/df(xl);
+        }
+        if(f(xr)*ddf(xr) < 0){
+            xr = xr - (f(xr)*(xr - xl) / (f(xr) - f(xl)));
+        }
+        else{
+            xr = xr - f(xr)/df(xr);
+        }
+    }
+    printf("Find root for %d steps\n",stepCount); //статистика
+    return (xl+xr)/2;
+}
 
 float f(float x)
 {
     return 8*x*x*x*x+32*x*x*x+40*x*x+16*x+1;
+}
+
+float df(float x)
+{
+    return 32*x*x*x+96*x*x+80*x+16;
+}
+
+float ddf(float x)
+{
+    return 96*x*x+192*x+80;
+}
+
+float f1(float x)
+{
+    return 0.6 * x + 3;
+}
+
+float df1(float x)
+{
+    x = 1.0;
+    return 0.6*x;
+}
+
+float ddf1(float x)
+{
+    x = 0.0;
+    return 0.6*x;
+}
+
+float f2(float x)
+{
+    return (x-2)*(x-2)*(x-2)-1;
+}
+
+float df2(float x)
+{
+    return 3*(x-2)*(x-2);
+}
+
+float ddf2(float x)
+{
+    return 6*x-12;
+}
+
+float f3(float x)
+{
+    return 3/x;
+}
+
+float df3(float x)
+{
+    return -3*pow(x,-2);
+}
+
+float ddf3(float x)
+{
+    return 6*pow(x,-3);
+}
+
+float testF(float x)
+{
+    return 2*x;
+}
+
+float calcIntegralSqu(float xl, float xr, size_t n, function f)
+{
+    float sum = 0;
+    float h = (xr-xl)/n;
+    for (size_t i = 0; i < n; i++)
+    {
+        sum += f(xl);
+        xl += h;
+    }
+    return sum*h;
+    
+}
+
+float calcIntegralTrap(float xl, float xr, size_t n, function f)
+{
+    float sum = 0.0;
+    float h = (xr-xl)/n;
+    for (float x = xl+h; x < xr-h; x+=h)
+    {
+        sum += 0.5*h*(f(x)+f(x+h));
+    }
+    return sum;
+}
+
+float calcIntegralSimpson(float xl, float xr, size_t n, function f)
+{
+    float sum = 0;
+    float h = (xr-xl)/n;
+    for (float x = xl+h; x < xr - h; x+=h)
+    {
+        sum += h/6.0*(f(x) + 4.0*f(0.5*(x+x+h)) + f(x+h));
+    }
+    return sum;
 }
--- curseCHIGH/func.h
+++ curseCHIGH/func.h
@@ -1,5 +1,37 @@
+#ifndef FUNC_H
+#define FUNC_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <math.h>
+#include <stdbool.h>
 
-typedef float(*function)(float);
-float rootFindLineSearch(float xl, float xr, float eps,function f);
+typedef float(*function)(float);        //Тип данных указателя на функцию
+
+int signF(float x, function f);         //Определение знака функции
+// Поиск нулей функция
+float rootFindLineSearch(float xl, float xr, float eps, function f);
+float rootFindDiv(float xl, float xr, float eps, function f);
+float rootFindDiv2(float xl, float xr, float eps, function f);
+float rootFindChord(float xl, float xr, float eps, function f);
+float rootFindTangent(float xn, float eps, function f, function df);
+float rootFindCombine(float xl, float xr, float eps, function f, function df, function ddf);
+
+//Заданная функция
 float f(float x);
+float df(float x);
+float ddf(float x);
+
+//Интегрирование, площадь под кривой
+float calcIntegralSqu(float xl, float xr, size_t n, function f);
+float calcIntegralTrap(float xl, float xr, size_t n, function f);
+float calcIntegralSimpson(float xl, float xr, size_t n, function f);
+
+
+
+
+#endif
+
+
